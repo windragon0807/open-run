025fc9437006a73f8cc8f6b97f19aefe100c77a1
jdchoi <cj9745@naver.com>
2024-05-06 19:51:31 +0900

    누락된 dependency 추가

--- src/components/register/nickname/useNicknameValidation.tsx
+++ src/components/register/nickname/useNicknameValidation.tsx
@@ -5,24 +5,27 @@ export const useNicknameValidation = () => {
   const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(null)
   const NICKNAME_EXIST_CHK_DELAY = 500
 
-  const handleNicknameChange = useCallback(async (nickname: string, setIsValid: (isValid: boolean | null) => void) => {
-    /** 닉네임이 두 글자 이상일 때만 유효성 검사 실시 */
-    if (nickname.length < 2) {
-      return
-    }
+  const handleNicknameChange = useCallback(
+    async (nickname: string, setIsValid: (isValid: boolean | null) => void) => {
+      /** 닉네임이 두 글자 이상일 때만 유효성 검사 실시 */
+      if (nickname.length < 2) {
+        return
+      }
 
-    /** 닉네임이 변경된 후 {NICKNAME_EXIST_CHK_DELAY}ms 뒤 중복 확인 api를 호출하도록 디바운싱 처리 */
-    clearTimeout(typingTimeout as NodeJS.Timeout)
+      /** 닉네임이 변경된 후 {NICKNAME_EXIST_CHK_DELAY}ms 뒤 중복 확인 api를 호출하도록 디바운싱 처리 */
+      clearTimeout(typingTimeout as NodeJS.Timeout)
 
-    const timeout = setTimeout(() => {
-      /** 중복 확인 API 호출 이후 data 변경 */
-      checkExistNickname({ nickname }).then((data) => {
-        console.log('data', data)
-      })
-    }, NICKNAME_EXIST_CHK_DELAY)
+      const timeout = setTimeout(() => {
+        /** 중복 확인 API 호출 이후 data 변경 */
+        checkExistNickname({ nickname }).then((data) => {
+          console.log('data', data)
+        })
+      }, NICKNAME_EXIST_CHK_DELAY)
 
-    setTypingTimeout(timeout)
-  }, [])
+      setTypingTimeout(timeout)
+    },
+    [typingTimeout],
+  )
 
   return { handleNicknameChange }
 }
