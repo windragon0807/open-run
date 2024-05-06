a2a221861452e10499bc92ed3b567529a2ee41a4
jdchoi <cj9745@naver.com>
2024-05-06 18:32:33 +0900

    닉네임 중복체크 api를 호출하는 함수에서 사용하는 Response타입을 커스텀 타입으로 변경

--- src/apis/nickname/checkExistNickname/api.ts
+++ src/apis/nickname/checkExistNickname/api.ts
@@ -1,5 +1,5 @@
 import http from '@/apis/axios'
-import { RequestType } from '@/apis/nickname/checkExistNickname/type'
+import { RequestType, ResponseType } from '@/apis/nickname/checkExistNickname/type'
 
 /**
  * 해당 닉네임이 중복된 닉네임인지 확인하는 API
