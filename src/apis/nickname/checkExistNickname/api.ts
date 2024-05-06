3e64cc21216ac1e44b07d7695f8ed788a2310064
jdchoi <cj9745@naver.com>
2024-05-06 18:26:43 +0900

    닉네임 중복 체크 api 요청 함수 파일 추가

---
+++ src/apis/nickname/checkExistNickname/api.ts
@@ -0,0 +1,13 @@
+import http from '@/apis/axios'
+import { RequestType } from '@/apis/nickname/checkExistNickname/type'
+
+/**
+ * 해당 닉네임이 중복된 닉네임인지 확인하는 API
+ */
+export default function checkExistNickname(params: RequestType): Promise<ResponseType> {
+  return http.get(`/v1/users/nickname/exist`, {
+    params: {
+      nickname: params.nickname,
+    },
+  })
+}
---
+++ src/apis/nickname/checkExistNickname/type.ts
@@ -0,0 +1,10 @@
+import { ApiResponse } from '@/apis/axios'
+
+export type RequestType = {
+  nickname: string
+}
+
+export type ResponseType = ApiResponse<{
+  message: string
+  data: boolean
+}>
