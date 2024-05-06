04d953b08939e0f065c59ad6ea1fc98e95c5c935
jdchoi <cj9745@naver.com>
2024-05-06 18:30:52 +0900

    기존 임시함수 제거 후 api 함수와 연결

--- src/components/register/nickname/useNicknameValidation.tsx
+++ src/components/register/nickname/useNicknameValidation.tsx
@@ -1,22 +1,9 @@
-import axios from '@/apis/axios'
+import checkExistNickname from '@/apis/nickname/checkExistNickname/api'
 import { useCallback, useState } from 'react'
 
 export const useNicknameValidation = () => {
   const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(null)
   const NICKNAME_EXIST_CHK_DELAY = 500
-
-  /**
-   * 닉네임 중복 검사 api 호출
-   * TODO: apis 폴더로 이동 예정
-   */
-  const checkExistNickname = async (nickname: string) => {
-    try {
-      /** API 호출 위치 */
-      console.log('checkExistNickname', nickname)
-    } catch (error) {
-      console.error(error)
-    }
-  }
 
   const handleNicknameChange = useCallback(async (nickname: string, setIsValid: (isValid: boolean | null) => void) => {
     /** 닉네임이 두 글자 이상일 때만 유효성 검사 실시 */
@@ -29,8 +16,8 @@ export const useNicknameValidation = () => {
 
     const timeout = setTimeout(() => {
       /** 중복 확인 API 호출 이후 data 변경 */
-      checkExistNickname(nickname).then((data) => {
-        // setIsValid(data)
+      checkExistNickname({ nickname }).then((data) => {
+        console.log('data', data)
       })
     }, NICKNAME_EXIST_CHK_DELAY)
 
