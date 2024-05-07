26317d514b34d9c5f7bbd8c18155efca580c12ac
jdchoi <cj9745@naver.com>
2024-05-07 10:35:22 +0900

    콜백체인 구조에서 await / try-catch 구조로 변경

--- src/components/register/nickname/useNicknameValidation.tsx
+++ src/components/register/nickname/useNicknameValidation.tsx
@@ -14,12 +14,15 @@ export const useNicknameValidation = () => {
 
       /** 닉네임이 변경된 후 {NICKNAME_EXIST_CHK_DELAY}ms 뒤 중복 확인 api를 호출하도록 디바운싱 처리 */
       clearTimeout(typingTimeout as NodeJS.Timeout)
-
-      const timeout = setTimeout(() => {
-        /** 중복 확인 API 호출 이후 data 변경 */
-        checkExistNickname({ nickname }).then((data) => {
-          console.log('data', data)
-        })
+      const timeout = setTimeout(async () => {
+        try {
+          /** 중복 확인 API 호출 이후 data 변경 */
+          const response = await checkExistNickname({ nickname })
+          console.log('response', response)
+        } catch (error) {
+          console.error(error)
+          throw error
+        }
       }, NICKNAME_EXIST_CHK_DELAY)
 
       setTypingTimeout(timeout)
