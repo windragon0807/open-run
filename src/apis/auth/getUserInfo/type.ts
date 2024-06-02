70cbbf0f0224ea10279efa8fcdb91a61d4aeeb1b
windragon0807 <tmdfyd1995@gmail.com>
2024-06-02 14:08:43 +0900

    홈 화면 수정

--- src/apis/auth/getUserInfo/type.ts
+++ src/apis/auth/getUserInfo/type.ts
@@ -1,3 +1,15 @@
 import { ApiResponse } from '@apis/axios'
 
-export type ResponseType = ApiResponse<{}>
+export type ResponseType = ApiResponse<{
+  userId: string
+  withdraw: boolean
+  nickname: string
+  email: string
+  identityAuthenticated: boolean
+  provider: string
+  createdDate: any
+  lastLoginDate: any
+  blockchainAddress: string
+  runningPace: string
+  runningFrequency: number
+}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import Home from '@components/home/Home'
+import Link from 'next/link'
 
-export default function HomePage() {
+import { getUserInfo } from '@apis/auth/getUserInfo/api'
+import Spacing from '@components/shared/Spacing'
+
+export default async function HomePage() {
+  const { data } = await getUserInfo()
+  console.log(data)
   return (
-    <main>
-      <Home />
-    </main>
+    <section className='w-full h-full flex flex-col items-center justify-center'>
+      <div className='p-20 border-2 border-primary rounded-8'>
+        <p className='text-center font-bold'>로그인 정보</p>
+        <Spacing size={16} />
+        <p>닉네임 : {data.nickname}</p>
+        <p>이메일 : {data.email}</p>
+        <p>로그인 : {data.provider}</p>
+        <p>페이스 : {data.runningPace}</p>
+        <p>횟수 : {data.runningFrequency}</p>
+      </div>
+      <Spacing size={40} />
+      <Link href='/register'>
+        <button className='px-20 py-10 text-white bg-primary rounded-8'>회원가입</button>
+      </Link>
+      <Spacing size={16} />
+      <Link href='/signin'>
+        <button className='px-20 py-10 text-white bg-primary rounded-8'>로그인</button>
+      </Link>
+    </section>
   )
 }
