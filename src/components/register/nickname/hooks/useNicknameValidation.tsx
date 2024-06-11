c74d5165ae973923c544292c7436bf156e88c59e
windragon0807 <tmdfyd1995@gmail.com>
2024-06-11 23:28:45 +0900

    input 입력 상태값 타임아웃으로 인한 오류 수정

--- src/components/register/nickname/hooks/useNicknameValidation.tsx
+++ src/components/register/nickname/hooks/useNicknameValidation.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import checkExistNickname from '@apis/auth/checkExistNickname/api'
 import { NicknameValidState } from '../types'
@@ -8,27 +8,34 @@ const NICKNAME_EXIST_CHK_DELAY = 500
 export const useNicknameValidation = () => {
   const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(null)
 
+  const clearUp = useCallback(() => {
+    clearTimeout(typingTimeout as NodeJS.Timeout)
+  }, [typingTimeout])
+
   const handleNicknameChange = useCallback(
     async (nickname: string, setIsValid: (isValid: NicknameValidState) => void) => {
       /** 닉네임이 두 글자 이상일 때만 유효성 검사 실시 */
       if (nickname.length < 2) {
+        clearUp()
         setIsValid('default')
         return
       }
 
       /** 두 글자 이상이지만 한글 초성만 입력된 경우, 유효성 실패로 인정 */
-      const isOnlyChosung = /^[ㄱ-ㅎ]+$/
+      const isOnlyChosung = /[ㄱ-ㅎ]/
       if (isOnlyChosung.test(nickname)) {
+        clearUp()
         setIsValid('consonant')
         return
       }
 
       /** 닉네임이 변경된 후 {NICKNAME_EXIST_CHK_DELAY}ms 뒤 중복 확인 api를 호출하도록 디바운싱 처리 */
-      clearTimeout(typingTimeout as NodeJS.Timeout)
+      clearUp()
       const timeout = setTimeout(async () => {
         try {
           /** 중복 확인 API 호출 이후 data 변경 */
           const response = await checkExistNickname({ nickname })
+          console.log('ryong', response)
           setIsValid(response.data === true ? 'overlap' : 'pass')
         } catch (error) {
           console.error(error)
@@ -37,8 +44,14 @@ export const useNicknameValidation = () => {
 
       setTypingTimeout(timeout)
     },
-    [typingTimeout],
+    [clearUp],
   )
+
+  useEffect(() => {
+    return () => {
+      clearUp()
+    }
+  })
 
   return { handleNicknameChange }
 }
