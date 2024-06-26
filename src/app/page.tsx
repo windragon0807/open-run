6b8bec282a4982dc189436ce130312b02ed14657
windragon0807 <tmdfyd1995@gmail.com>
2024-06-26 16:53:15 +0900

    로그인하지 않은 상태에서 접속 시 화면이 죽는 이슈 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,26 +9,36 @@ import DeleteButton from '@components/home/DeleteButton'
 export default async function HomePage() {
   const token = cookies().get('ACCESSTOKEN')?.value
 
+  if (token == null) {
+    return (
+      <section className='w-full h-full flex flex-col items-center justify-center'>
+        <Link href='/register'>
+          <button className='px-20 py-10 text-white bg-primary rounded-8'>회원가입</button>
+        </Link>
+        <Spacing size={16} />
+        <Link href='/signin'>
+          <button className='px-20 py-10 text-white bg-primary rounded-8'>로그인</button>
+        </Link>
+      </section>
+    )
+  }
+
   const { data } = await getUserInfo()
   console.log('userInfo', data)
 
   return (
     <section className='w-full h-full flex flex-col items-center justify-center'>
-      {token != null ? (
-        <>
-          <div className='p-20 border-2 border-primary rounded-8'>
-            <p className='text-center font-bold'>로그인 정보</p>
-            <Spacing size={16} />
-            <p>닉네임 : {data.nickname}</p>
-            <p>이메일 : {data.email}</p>
-            <p>로그인 : {data.provider}</p>
-            <p>페이스 : {data.runningPace}</p>
-            <p>횟수 : {data.runningFrequency}</p>
-            <p>생성일 : {data.createdDate}</p>
-          </div>
-          <Spacing size={40} />
-        </>
-      ) : null}
+      <div className='p-20 border-2 border-primary rounded-8'>
+        <p className='text-center font-bold'>로그인 정보</p>
+        <Spacing size={16} />
+        <p>닉네임 : {data.nickname}</p>
+        <p>이메일 : {data.email}</p>
+        <p>로그인 : {data.provider}</p>
+        <p>페이스 : {data.runningPace}</p>
+        <p>횟수 : {data.runningFrequency}</p>
+        <p>생성일 : {data.createdDate}</p>
+      </div>
+      <Spacing size={40} />
       <Link href='/register'>
         <button className='px-20 py-10 text-white bg-primary rounded-8'>회원가입</button>
       </Link>
