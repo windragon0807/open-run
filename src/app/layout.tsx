7faf1f2a19490de88ebde43a3194333d45e72251
windragon0807 <tmdfyd1995@gmail.com>
2024-07-15 16:50:06 +0900

    모바일 웹 풀스크린 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,12 @@ import '@styles/globals.css'
 export const metadata: Metadata = {
   title: 'OpenRun',
   description: "OpenRun, Let's run together!",
+  other: {
+    /* Allow web app to be run in full-screen mode - iOS. */
+    'apple-mobile-web-app-capable': 'yes',
+    /* Allow web app to be run in full-screen mode - Android. */
+    'mobile-web-app-capable': 'yes',
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
