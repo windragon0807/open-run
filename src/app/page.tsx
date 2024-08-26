ffbca32491b95474617b45d5bb96270ca6d3dc5f
windragon0807 <tmdfyd1995@gmail.com>
2024-08-26 18:33:58 +0900

    홈 화면 로그인되어 있지 않을 시, 로그인 페이지로 리다이렉트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
+
 export default async function HomePage() {
-  return <div></div>
+  const token = cookies().get('ACCESSTOKEN')?.value
+
+  if (token == null) {
+    redirect('/signin')
+  }
+
+  return <div>Home</div>
 }
