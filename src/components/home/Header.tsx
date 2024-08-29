542e78db7ca848f2b4c01783e98912b57eee10e5
windragon0807 <tmdfyd1995@gmail.com>
2024-08-29 16:22:00 +0900

    뷰포트 너비 늘릴 시, header 요소 일부 짤리는 현상 수정

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -3,7 +3,7 @@ import Avartar from './Avartar'
 
 export default function Header() {
   return (
-    <header className='fixed z-[100] bg-gray-lighten w-full h-84 flex justify-between p-[16px_12px_16px_16px]'>
+    <header className='fixed z-[100] bg-gray-lighten w-full max-w-tablet h-84 flex justify-between p-[16px_12px_16px_16px]'>
       <div className='flex flex-col'>
         <span className='text-[12px] leading-[16px] tracking-[-0.24px]'>칭호가 들어갑니다</span>
         <span className='text-[28px] leading-[36px] tracking-[-0.56px] font-bold'>Username</span>
