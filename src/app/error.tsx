70a49fc04df2b0f8c23a539bf9ad6edc1301d95b
windragon0807 <tmdfyd1995@gmail.com>
2024-10-20 21:08:34 +0900

    에러 페이지 개발

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-
-import { removeCookie } from '@utils/cookie'
-import { COOKIE } from '@constants/cookie'
+import { HiHome } from 'react-icons/hi'
+import { IoWarning } from 'react-icons/io5'
+import { MdOutlineRefresh } from 'react-icons/md'
+import Link from 'next/link'
 
 type Props = {
   error: Error
@@ -12,23 +11,23 @@ type Props = {
 }
 
 export default function ErrorPage({ error, reset }: Props) {
-  const router = useRouter()
-
-  useEffect(() => {
-    console.error('ryong', error)
-  }, [error])
-
   return (
-    <div>
-      <h2>Something went wrong!</h2>
+    <section className='w-dvw h-dvh flex flex-col justify-center items-center bg-primary'>
+      <IoWarning className='mb-20' size={80} color='var(--secondary)' />
+      <h2 className='text-2xl font-bold text-white mb-10'>앗! 문제가 발생했어요</h2>
+      <p className='text-lg text-white mb-40'>잠시 후 다시 시도해 주세요.</p>
       <button
-        onClick={() => {
-          // reset()
-          removeCookie(COOKIE.ACCESSTOKEN)
-          router.replace('/')
-        }}>
-        Try again
+        className='px-20 py-10 bg-white text-primary rounded-4 flex justify-center items-center gap-8 font-[500] mb-15'
+        onClick={reset}>
+        <MdOutlineRefresh size={18} />
+        다시 시도하기
       </button>
-    </div>
+      <Link
+        className='px-20 py-10 bg-white text-primary rounded-4 flex justify-center items-center gap-8 font-[500]'
+        href='/'>
+        <HiHome size={18} />
+        홈으로 돌아가기
+      </Link>
+    </section>
   )
 }
--- src/components/icons/ArrowRight.tsx
+++ src/components/icons/ArrowRight.tsx
@@ -1,7 +1,7 @@
-export default function ArrowRight() {
+export default function ArrowRight({ className }: { className?: string }) {
   return (
-    <svg width='24' height='24' viewBox='0 0 24 24' fill='none'>
-      <path className='fill-dark dark:fill-white' d='M12.6 12L8 7.4L9.4 6L15.4 12L9.4 18L8 16.6L12.6 12Z' />
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' className={className}>
+      <path className='fill-black dark:fill-white' d='M12.6 12L8 7.4L9.4 6L15.4 12L9.4 18L8 16.6L12.6 12Z' />
     </svg>
   )
 }
