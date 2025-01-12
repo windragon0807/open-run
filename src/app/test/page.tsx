64d04ff2fc04491d7e75f5f01e7e78cb861ff017
windragon0807 <tmdfyd1995@gmail.com>
2025-01-12 23:23:50 +0900

    테스트 컴포넌트 삭제

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import TestClient from '@components/temp/TestClient'
-
-export default function TestPage() {
-  return (
-    <section className='flex items-center justify-center w-dvw h-dvh bg-primary'>
-      <TestClient />
-    </section>
-  )
-}
