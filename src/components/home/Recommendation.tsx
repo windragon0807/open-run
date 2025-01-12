40da0219de1585c401e31fdbf969fcc4992c51ec
windragon0807 <tmdfyd1995@gmail.com>
2025-01-12 23:41:34 +0900

    추천 버튼 비활성화

--- src/components/home/Recommendation.tsx
+++ src/components/home/Recommendation.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 
 import { convertStringTimeToDate } from '@utils/time'
 import Spacing from '@shared/Spacing'
-import ArrowRight from '@icons/ArrowRight'
 import { fetchBungs } from '@apis/bungs/fetchBungs/api'
 import RecommendationCard from './RecommendationCard'
 
@@ -15,10 +14,9 @@ export default async function Recommendation() {
 
   return (
     <section className='px-16 flex flex-col'>
-      <button className='flex justify-between items-center w-full max-w-[500px] mx-auto'>
+      <div className='flex justify-between items-center w-full max-w-[500px] mx-auto'>
         <span className='text-[20px] font-bold leading-[30px] tracking-[-0.4px] text-black dark:text-white'>추천</span>
-        <ArrowRight />
-      </button>
+      </div>
       <section className='flex flex-col gap-8'>
         {recommendationList?.map((item) => (
           <Link href={`/bung/${item.bungId}`} key={item.bungId}>
