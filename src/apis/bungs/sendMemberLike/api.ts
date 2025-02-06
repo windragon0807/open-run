ad94715d0988dd0c9e103a75fdcc065e60b526a5
windragon0807 <tmdfyd1995@gmail.com>
2025-02-06 10:36:26 +0900

    피드백 API 오류 수정 대응

--- src/apis/bungs/sendMemberLike/api.ts
+++ src/apis/bungs/sendMemberLike/api.ts
@@ -2,5 +2,5 @@ import http from '@apis/axios'
 import { RequestType } from './type'
 
 export function sendMemberLike(params: RequestType) {
-  return http.get('/v1/users/feedback', { params })
+  return http.patch('/v1/users/feedback', params)
 }
