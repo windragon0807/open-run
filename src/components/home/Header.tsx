0de8140cbc0c256d86038a889a78c1af0b1c3405
windragon0807 <tmdfyd1995@gmail.com>
2025-02-17 16:46:29 +0900

    헤더 내 이름 수직 가운데 정렬 처리

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -12,10 +12,8 @@ export default function Header() {
   const { logout } = useLogout()
 
   return (
-    <header className='fixed z-[100] bg-gray-lighten w-full max-w-tablet h-84 flex justify-between p-[16px_12px_16px_16px]'>
-      <div className='flex flex-col'>
-        <span className='text-[28px] leading-[36px] tracking-[-0.56px] font-bold'>{userInfo?.nickname}</span>
-      </div>
+    <header className='fixed z-[100] bg-gray-lighten w-full max-w-tablet h-84 flex items-center justify-between p-[16px_12px_16px_16px]'>
+      <span className='text-[28px] leading-[36px] tracking-[-0.56px] font-bold'>{userInfo?.nickname}</span>
       <div className='flex items-center gap-[15px]'>
         <BellIcon size={24} color={colors.black.darken} />
         <MagnifierIcon size={24} color={colors.black.darken} />
