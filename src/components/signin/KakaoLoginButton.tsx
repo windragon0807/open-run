f988a3a6f9f6d6f91407916d76e0700bbe0a904e
windragon0807 <tmdfyd1995@gmail.com>
2025-02-17 16:19:06 +0900

    로그인 버튼 hover 효과 추가

--- src/components/signin/KakaoLoginButton.tsx
+++ src/components/signin/KakaoLoginButton.tsx
@@ -2,7 +2,9 @@ import Image from 'next/image'
 
 export default function KakaoLoginButton({ onClick }: { onClick?: () => void }) {
   return (
-    <button className='flex gap-8 w-full h-[56px] bg-kakao justify-center items-center rounded-8' onClick={onClick}>
+    <button
+      className='flex gap-8 w-full h-[56px] bg-kakao hover:bg-kakao/80 justify-center items-center rounded-8'
+      onClick={onClick}>
       <Image src='/images/icon_kakao.png' width={24} height={24} alt='카카오 아이콘' />
       <span className='text-base font-bold'>카카오톡으로 시작하기</span>
     </button>
--- src/components/signin/NaverLoginButton.tsx
+++ src/components/signin/NaverLoginButton.tsx
@@ -2,7 +2,9 @@ import Image from 'next/image'
 
 export default function NaverLoginButton({ onClick }: { onClick?: () => void }) {
   return (
-    <button className='flex gap-8 w-full h-[56px] bg-naver justify-center items-center rounded-8' onClick={onClick}>
+    <button
+      className='flex gap-8 w-full h-[56px] bg-naver hover:bg-naver/80 justify-center items-center rounded-8'
+      onClick={onClick}>
       <Image src='/images/icon_naver.png' width={24} height={24} alt='네이버 아이콘' />
       <span className='text-base font-bold text-white'>네이버로 시작하기</span>
     </button>
