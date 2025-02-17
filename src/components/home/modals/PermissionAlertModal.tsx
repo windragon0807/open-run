5f1f8031849a1e7a6ab1a5ee1c97cb23d70c20a2
windragon0807 <tmdfyd1995@gmail.com>
2025-02-17 17:35:37 +0900

    위치 권한이 거부 상태일 때 뜨는 모달 디자인 변경

--- src/components/home/modals/PermissionAlertModal.tsx
+++ src/components/home/modals/PermissionAlertModal.tsx
@@ -1,19 +1,19 @@
-import { useModalContext } from '@contexts/ModalContext'
 import { Popup } from '@shared/Modal'
+import { useModalContext } from '@contexts/ModalContext'
+import BrokenXIcon from '@icons/BrokenXIcon'
+import { colors } from '@styles/colors'
 
 export default function PermissionAlertModal() {
   const { closeModal } = useModalContext()
   return (
     <Popup>
-      <div className='w-full h-200 flex flex-col justify-between items-center p-16'>
-        <div className='flex flex-col gap-8 mt-24'>
-          <h5 className='text-20 leading-30 font-bold text-black-darken text-center'>위치 권한 확인</h5>
-          <p className='text-black-darken text-sm text-center'>위치 권한을 확인해주세요</p>
-        </div>
-        <div className='w-full flex gap-8'>
-          <button className='flex-1 h-56 bg-white text-black-darken text-base font-bold rounded-8' onClick={closeModal}>
-            확인
-          </button>
+      <div className='relative w-full flex flex-col justify-between items-center p-16 pt-40'>
+        <button className='absolute top-16 right-16' onClick={closeModal}>
+          <BrokenXIcon size={24} color={colors.black.darken} />
+        </button>
+        <div className='flex flex-col gap-8 mb-20'>
+          <h5 className='text-20 leading-30 font-bold text-black-darken text-center'>서비스 접근 권한 안내</h5>
+          <p className='text-black-darken text-sm text-center'>{`위치 권한 사용을 거부하였습니다. 기능 사용을 원하실 경우 휴대폰설정 > 애플리케이션 관리자에서 해당 앱의 권한을 허용해주세요.`}</p>
         </div>
       </div>
     </Popup>
