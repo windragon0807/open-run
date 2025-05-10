9eda627dfa62b51a064a90bfd5c41c5cc6d97137
windragon0807 <tmdfyd1995@gmail.com>
2025-05-10 10:19:23 +0900

    feature: 서비스 내에서 지도가 열리지 않을 때 웹으로 리다이렉트할 수 있는 기능 추가

--- src/components/bung/Map.tsx
+++ src/components/bung/Map.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from 'next/image'
+import Link from 'next/link'
 import { memo } from 'react'
 import { Container, Marker, NaverMap, useNavermaps } from 'react-naver-maps'
 import { useGeocode } from '@apis/maps/fetchGeocode/query'
@@ -13,6 +14,12 @@ function Map({ location }: { location: string }) {
     return (
       <div className='relative h-200 w-full animate-pulse'>
         <Image className='object-cover' src='/images/maps/map_placeholder.png' alt='map' fill />
+        <Link
+          href={`https://map.naver.com/v5/search/${encodeURIComponent(location)}`}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='bg-black/50 hover:bg-black/60 absolute inset-0 flex items-center justify-center text-white transition-colors'
+        />
       </div>
     )
 
