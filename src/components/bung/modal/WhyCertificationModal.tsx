3ef9c19ce038c5936854dd72327d76cfe6678e80
windragon0807 <tmdfyd1995@gmail.com>
2025-06-12 21:57:49 +0900

    feature: 참여 인증은 왜 해야 하나요? 문구 추가

--- src/components/bung/modal/WhyCertificationModal.tsx
+++ src/components/bung/modal/WhyCertificationModal.tsx
@@ -6,15 +6,25 @@ export default function WhyCertificationModal() {
   const { closeModal } = useModalContext()
   return (
     <section
-      className='absolute left-1/2 top-1/2 h-[70%] w-[90%] max-w-[328px] -translate-x-1/2 -translate-y-1/2 rounded-8 bg-white'
+      className='absolute left-1/2 top-1/2 w-[90%] max-w-[328px] -translate-x-1/2 -translate-y-1/2 rounded-8 bg-white'
       onClick={(e) => e.stopPropagation()}>
       <div className='h-full w-full'>
-        <header className='relative flex h-60 w-full items-center justify-center'>
+        <header className='relative mb-16 flex h-60 w-full items-center justify-center'>
           <h3 className='text-16 font-bold text-black-darken'>참여 인증을 왜 해야 하나요?</h3>
           <button className='absolute right-16' onClick={closeModal}>
             <BrokenXIcon size={24} color={colors.black.default} />
           </button>
         </header>
+        <div className='px-24 pb-30 text-14'>
+          <h4 className='mb-4 font-bold'>참여 인증이 뭔가요?</h4>
+          <p className='mb-24'>
+            모임 장소 도착 여부를 인증하는 절차입니다.
+            <br />
+            참여 인증을 통해 실제로 참여한 멤버를 확인하고, 러닝 이후 피드백을 남기거나 보상을 받을 수 있어요.
+          </p>
+          <h4 className='mb-4 font-bold'>언제 하면 되나요?</h4>
+          <p>러닝 시작 전, 약속 장소에 모든 멤버가 모이면 참여 인증을 진행해 주세요.</p>
+        </div>
       </div>
     </section>
   )
