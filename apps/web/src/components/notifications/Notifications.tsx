5fba99e9c589f9b03e3889ebc23d416db06f4b30
windragon0807 <tmdfyd1995@gmail.com>
2026-06-09 23:30:46 +0900

    feat: 알림 빈 상태 디자인 개선
    - 홈 추천 영역의 빈 상태 이미지를 알림 페이지에 적용
    
    - 알림 없음 안내 문구를 이미지와 함께 2줄로 구성
    
    - 상단 여백 없이 본문 중앙에 보이도록 정렬

--- apps/web/src/components/notifications/Notifications.tsx
+++ apps/web/src/components/notifications/Notifications.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { ArrowLeftIcon } from '@icons/arrow'
 import { colors } from '@styles/colors'
@@ -19,7 +20,13 @@ export default function Notifications() {
       </header>
 
       <div className='h-[calc(100%-60px)] overflow-y-auto px-16 pb-120'>
-        <div className='mt-80 text-center text-14 leading-20 text-gray-darken'>아직 알림이 없어요</div>
+        <div className='flex h-full w-full flex-col items-center justify-center gap-8'>
+          <Image src='/images/home/skewed_x_button.png' alt='기울어진 X 버튼 이미지' width={56} height={56} />
+          <p className='text-center text-14 leading-20 text-gray-darken'>
+            아직 알림이 없어요 <br />
+            새로운 알림이 오면 여기서 확인할 수 있어요!
+          </p>
+        </div>
       </div>
     </section>
   )
